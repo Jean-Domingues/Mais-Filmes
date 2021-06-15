aada15d08230bf31bff2b2310fea35a830672a71
Jean Domigues <jeandomingues752@gmail.com>
2021-06-15 20:45:18 -0300

    Retirando variáveis não usadas

--- src/pages/Filmes/index.js
+++ src/pages/Filmes/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Title } from './style';
 
 import { Loader } from '../../style';
